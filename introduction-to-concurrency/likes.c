2eb58bd4334ed48d29acb5683f9ab58d0069cb63
Christopher R. Johnson <crjohns@csail.mit.edu>
2013-03-11 15:56:56 -0400

    Added "likes" example

---
+++ introduction-to-concurrency/likes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int likes = 0;
+
+void like_post()
+{
+    likes += 1;
+}
+
+
+void *like_test(void *arg)
+{
+    for(int i=0; i<10000; i++)
+        like_post();
+
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    /* Create two threads that each run the function "like_test", and
+     * then wait for both of them to finish running before exiting */
+    pthread_t t1;
+    pthread_t t2;
+
+    pthread_create(&t1, NULL, like_test, NULL);
+    pthread_create(&t2, NULL, like_test, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Post was liked %d times\n", likes);
+
+    return 0;
+}
